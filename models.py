73e6266582935a6bfe2599a41395f69199f91645
Niel_hu <10646097+TengHu@users.noreply.github.com>
2018-05-29 11:19:16 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+import pdb
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 class RNN(nn.Module):
-
     def __init__(self, feature_size, hidden_size):
         super(DLSTM3, self).__init__()
         self.feature_size = feature_size
@@ -15,7 +18,6 @@ class RNN(nn.Module):
         return F.log_softmax(outputs, 2)
 
     def initHidden(self, layer=1, batch_size=50, use_gpu=True):
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         h = torch.randn(1, batch_size, self.hidden_size).pin_memory()
         if use_gpu:
             h = h.to(device)
@@ -42,36 +44,48 @@ class DLSTM3(nn.Module):
             num_layers=1,
             batch_first=True)
         self.h2 = nn.LSTM(
-            input_size=feature_size+hidden_size,
+            input_size=feature_size + hidden_size,
             hidden_size=hidden_size,
             num_layers=1,
             batch_first=True)
         self.h3 = nn.LSTM(
-            input_size=feature_size+hidden_size,
+            input_size=feature_size + hidden_size,
             hidden_size=hidden_size,
             num_layers=1,
             batch_first=True)
 
         self.h2o = nn.Linear(hidden_size * 3, feature_size)
 
-    def forward(self, input, hiddens):
+    def forward(self, inputs, hiddens):
         [h1, h2, h3] = hiddens
-
-        h1_outputs, h1 = self.h1(input, h1)
-        h2_outputs, h2 = self.h2(torch.cat((input, h1_outputs), 2), h2)
-        h3_outputs, h3 = self.h3(torch.cat((input, h2_outputs), 2), h3)
-
+        inputs = self.OneHotEncoding(inputs)
+        h1_outputs, h1 = self.h1(inputs, h1)
+        h2_outputs, h2 = self.h2(torch.cat((inputs, h1_outputs), 2), h2)
+        h3_outputs, h3 = self.h3(torch.cat((inputs, h2_outputs), 2), h3)
         outputs = self.h2o(torch.cat((h1_outputs, h2_outputs, h3_outputs), 2))
-        return F.log_softmax(outputs, 2), hiddens
+        return F.log_softmax(outputs, 2), [h1, h2, h3]
 
-    def initHidden(self, layer=1, batch_size=50, use_gpu=True):
+    def initHidden(self, layer=1, batch_size=50):
         hidden_layers = []
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         for i in range(0, layer):
-            h = torch.randn(1, batch_size, self.hidden_size).pin_memory()
-            c = torch.randn(1, batch_size, self.hidden_size).pin_memory()
-            if use_gpu:
-                h = h.to(device)
-                c = c.to(device)
+            h = torch.randn(1, batch_size,
+                            self.hidden_size).to(device)
+            c = torch.randn(1, batch_size,
+                            self.hidden_size).to(device)
             hidden_layers.append((h, c))
         return hidden_layers
+
+    def OneHotEncoding(self, inputs):
+        '''
+        Input: (batch_size, sequence)
+        Output: (batch_size, sequence, features)
+        '''
+        batch_size = inputs.shape[0]
+        sequence = inputs.shape[1]
+        tensor = np.zeros(
+            (batch_size, sequence, self.feature_size), dtype=np.float32)
+        for i in range(0, batch_size):
+            for j in range(0, sequence):
+                tensor[i][j][inputs[i][j]] = 1
+
+        return torch.Tensor(tensor).pin_memory().to(device)
